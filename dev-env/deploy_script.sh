f1769b2e3b5cfeea34d6342eac2ee0e270a4fe18
shachar <shachar.meretz@brain.space>
2022-11-30 14:48:51 +0200

    docekr file

--- dev-env/deploy_script.sh
+++ dev-env/deploy_script.sh
@@ -3,15 +3,20 @@
 #It clones the repository and then checkout for the specific commit if added. 
 #After, it runs the cont_execute script in the brie repository. 
 
-git clone git@github.com:eeg-sense/containerise-cloud-environment.git
-
-git fetch
-
 echo "commit_version $COMMIT_HASH"
 echo "db_port $DB_PORT"
 echo "http_port $HTTP_PORT"
 
-#reate a DB in the container and then copy all the data from prod to the DB
+docker run --name postgres-db -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+
+git clone git@github.com:eeg-sense/containerise-cloud-environment.git
+
+git clone git@github.com:eeg-sense/kosmik.git
+git fetch
 [[ ! -z "$COMMIT_HASH" ]] && git checkout $COMMIT_HASH
-        
+
+
+#create a DB in the container and then copy all the data from prod to the DB
+#run the serverless offline 
+
 python3 cont_execute.py

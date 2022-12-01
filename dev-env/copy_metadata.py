2673367a7ad447cef75ff144f89d8b7c7a965bf4
shachar <shachar.meretz@brain.space>
2022-12-01 15:04:04 +0200

    after fix

--- dev-env/copy_metadata.py
+++ dev-env/copy_metadata.py
@@ -2,6 +2,8 @@ import logging
 from sqlalchemy.inspection import inspect
 import sqlalchemy.orm as orm
 from handle_metadata import DBConnector,create_engine_local
+import json
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -30,6 +32,8 @@ DB_PASSWORD_LOCAL="shachar"
 forgein_keys_arr={}
 
 def recursive_func(session,inspector,table_name,ids_arr=None):
+    
+
     forgein_keys=inspector.get_foreign_keys(table_name=table_name)
     
     if len(forgein_keys)==0:
@@ -38,8 +42,9 @@ def recursive_func(session,inspector,table_name,ids_arr=None):
     for forgein_key in forgein_keys: ##all the relation to this class
         all_forgein_keys=[]
         forgein_table_name=forgein_key["referred_table"]
+    
         if forgein_keys_arr.get(forgein_table_name) is None:
-                forgein_keys_arr[forgein_table_name]=[]
+                    forgein_keys_arr[forgein_table_name]=[]
         local_column=forgein_key["constrained_columns"][0]
         for obj_id in ids_arr: ##all the object in this class
             forgein_key_ids = session.execute(f"SELECT {local_column} FROM {table_name} WHERE id = {obj_id}").all()[0]
@@ -48,20 +53,20 @@ def recursive_func(session,inspector,table_name,ids_arr=None):
             elif not isinstance(forgein_key_ids,list):
                 all_forgein_keys.append(forgein_key_ids[0])
                 if forgein_key_ids[0] not in forgein_keys_arr[forgein_table_name]:
-                    forgein_keys_arr[forgein_table_name].append(forgein_key_ids[0])
+                        forgein_keys_arr[forgein_table_name].append(forgein_key_ids[0])
                 recursive_func(session,inspector,forgein_table_name,all_forgein_keys)
             else:
                 for forgein_key_id in forgein_key_ids:
                     all_forgein_keys.append(forgein_key_id[0])
                     if forgein_key_id[0] not in forgein_keys_arr[forgein_table_name]:
-                        forgein_keys_arr[forgein_table_name].append(forgein_key_id[0])
+                            forgein_keys_arr[forgein_table_name].append(forgein_key_id[0])
                 recursive_func(session,inspector,forgein_table_name, all_forgein_keys)
-        
+            
 
-def call_recursive(from_engine, measurements_id):
+def call_recursive(from_engine):
     inspector = inspect(from_engine) 
     with orm.Session(from_engine) as from_session:
-        eeg_samples = from_session.execute(f"SELECT id FROM eeg_sample where measurement_id = '{measurements_id[0]}'").all()
+        eeg_samples = from_session.execute(f"SELECT id FROM eeg_sample where measurement_successful=True ORDER BY usage_time ASC LIMIT 10").all()
         eeg_samples_ids=[]
         for eeg_sample in eeg_samples:
             if forgein_keys_arr.get('eeg_sample') is None:
@@ -70,14 +75,15 @@ def call_recursive(from_engine, measurements_id):
             eeg_samples_ids.append(eeg_sample[0])
         recursive_func(from_session,inspector, "eeg_sample", eeg_samples_ids)
 
-
-def metadata_copy(measurements_id):
+def metadata_copy():
     
-
+    tables=["sensi_event_type",#"analysis_type","analysis_version",
+    "measurement_flag_type","question_options"]
+                               
     try:
         from_engine = create_engine_local(PROD_TUNNEL_HOST,PROD_DB_HOST,PROD_PORT,PROD_DB_URI)
 
-        call_recursive(from_engine,measurements_id)
+        call_recursive(from_engine)
         
         OBJECTS=[]
 
@@ -85,26 +91,50 @@ def metadata_copy(measurements_id):
         from_cur = from_conn.cursor()
 
         for val in forgein_keys_arr:
+            from_cur.execute(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{val}';")
+            from_conn.connection.commit()
+            columns_type=from_cur.fetchall()
             if not len(forgein_keys_arr[val])==0:
-                from_cur.execute(f"SELECT * FROM {val} where id = ANY(ARRAY{forgein_keys_arr[val]})")
+                from_cur.execute(f"SELECT * FROM {val} WHERE id = ANY(ARRAY{forgein_keys_arr[val]})")
                 from_conn.connection.commit()
                 full_objects=from_cur.fetchall()
-                for object in full_objects:
-                    OBJECTS.append((val,object))
+                index=0
+                for column_name,column_type in columns_type:
+                    if column_type=="jsonb" or column_type=="json":
+                        for index2,obj in enumerate(full_objects):
+                            list_obj=list(obj)
+                            dict_obj=list_obj[index]
+                            dict_obj=json.dumps(dict_obj, indent = 4)
+                            list_obj[index]=dict_obj
+                            full_objects[index2]=tuple(list_obj)
+                    index+=1
+                for obj in full_objects:
+                    OBJECTS.append((val,obj))
+        
+        OBJECTS= OBJECTS[::-1]
+
+        for table in tables:
+            query=f"SELECT * FROM {table}"
+            print(query)
+            from_cur.execute(query)
+            from_conn.connection.commit()
+            full_objects=from_cur.fetchall()
+            for obj in full_objects:
+                OBJECTS.append((table,obj))
 
         conn = DBConnector(
                 ssh_host=STAGING_TUNNEL_HOST, db_name=STAGING_DB, db_user=STAGING_USER, db_password=STAGING_DB_PASSWORD, db_host=STAGING_DB_HOST, db_port=STAGING_PORT)
         conn.connect()
         cur=conn.connection.cursor()
-        for table_name, object in OBJECTS:
-                cur.execute(f"INSERT INTO {table_name} VALUES %s " , (object,))
-                conn.connection.commit()
-                cur.fetchall()
+        for table_name, obj in OBJECTS:
+            cur.execute(f"INSERT INTO {table_name} VALUES %s " , (obj,))
+        conn.connection.commit()
+                #cur.fetchall()
 
     except Exception as e:
         logger.error(e)
 
 
 if __name__ == '__main__':
-    measurement_ids=["KPZuZmzN"]
-    response = metadata_copy(measurement_ids)
+    #measurement_ids=["KPZuZmzN"]
+    response = metadata_copy()
